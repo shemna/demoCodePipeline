0e79d4789a6bf0d94ea89d88ce0d71b0f1c4f1e4
shemna <beevishemna@gmail.com>
2017-06-14 17:12:32 +0530

    Added logic for graceful exit

--- worker.js
+++ worker.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var egs = require('express-graceful-shutdown');
 var bodyParser = require('body-parser');
-
-app.use(egs(server, { forceTimeout: 30000 }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,5 +15,27 @@ app.get('/test', function(req, res, next){
 app.use(function(req, res, next) {
     res.status(400);
 });
+
+// this function is called when you want the server to die gracefully
+// i.e. wait for existing connections
+var gracefulShutdown = function() {
+  console.log("Received kill signal, shutting down gracefully.");
+  server.close(function() {
+    console.log("Closed out remaining connections.");
+    process.exit()
+  });
+  
+   // if after 
+   setTimeout(function() {
+       console.error("Could not close connections in time, forcefully shutting down");
+       process.exit()
+  }, 10*1000);
+}
+
+// listen for TERM signal .e.g. kill 
+process.on ('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on ('SIGINT', gracefulShutdown); 
 
 server.listen(3000);
