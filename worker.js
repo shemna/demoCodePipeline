25355bed97e452a7ddd82e0a3e9aea30d3ca5924
shemna <beevishemna@gmail.com>
2017-06-14 12:58:17 +0530

    Added changes for zero downtime deployment

--- worker.js
+++ worker.js
@@ -1,9 +1,11 @@
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-
+var egs = require('express-graceful-shutdown');
 var bodyParser = require('body-parser');
+
+app.use(egs(server, { forceTimeout: 30000 }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
